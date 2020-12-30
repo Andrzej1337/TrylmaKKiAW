8384641f66f30586df71cda4e9fb3a2d5d90c2ad
masterchlop <254661@student.pwr.edu.pl>
2020-12-30 23:03:24 +0100

    message

--- Klient/src/main/Main.java
+++ Klient/src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource( "board.fxml" ));
-        primaryStage.setTitle("Trylma - chińskie warcaby");
+        primaryStage.setTitle("Trylma");
         primaryStage.setResizable( false );
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
